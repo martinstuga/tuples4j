61a08275e447a6141c3cbdf285f8ab3f977d021d
Paulo Martins <martins.tuga@gmail.com>
2015-12-25 00:34:29 +0000

    Minimal changes

--- src/main/java/com/mtuga/tuples4j/Single.java
+++ src/main/java/com/mtuga/tuples4j/Single.java
@@ -6,76 +6,66 @@ import java.util.List;
 import com.mtuga.tuples4j.utils.EqualsUtils;
 
 
-@SuppressWarnings("serial")
 public class Single<A> extends Tuple {
 
 
-	protected A first = null;
+    protected A first = null;
 
 
-	public Single()
-	{
-	}
+    public Single() {
+    }
 
 
-	public Single(A first)
-	{
-		this.first = first;
-	}
+    public Single(A first) {
+        this.first = first;
+    }
 
 
-	@Override
-	public List<Object> asList()
-	{
-		List<Object> lst = super.asList();
-		lst.add(this.getFirst());
+    @Override
+    public List<Object> asList() {
+        List<Object> lst = super.asList();
+        lst.add(this.getFirst());
 
-		return lst;
-	}
-
-
-	public A getFirst()
-	{
-		return first;
-	}
+        return lst;
+    }
 
 
-	public void setFirst(A first)
-	{
-		this.first = first;
-	}
+    public A getFirst() {
+        return first;
+    }
 
 
-	public boolean isFullFilled()
-	{
-		return (this.getFirst() != null);
-	}
+    public void setFirst(A first) {
+        this.first = first;
+    }
 
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (!(this.getClass().isInstance(obj)))
-		{
-			return false;
-		}
-
-		Single<?> s = (Single<?>) obj;
-
-		if (EqualsUtils.areEqual(this.getFirst(), s.getFirst()))
-		{
-			return true;
-		}
-
-		return false;
-	}
+    @Override
+    public boolean isFullFilled() {
+        return (this.getFirst() != null);
+    }
 
 
-	@Override
-	public int hashCode()
-	{
-		int hash = 7;
-		hash = 37 * hash + (this.first != null ? this.first.hashCode() : 0);
-		return hash;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (!(this.getClass().isInstance(obj))) {
+            return false;
+        }
+
+        Single<?> s = (Single<?>) obj;
+
+        if (EqualsUtils.areEqual(this.getFirst(), s.getFirst())) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + (this.first != null ? this.first.hashCode() : 0);
+        return hash;
+    }
 }
--- src/main/java/com/mtuga/tuples4j/Tuple.java
+++ src/main/java/com/mtuga/tuples4j/Tuple.java
@@ -4,11 +4,21 @@ import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
-@SuppressWarnings("serial")
 public abstract class Tuple implements Serializable {
 
+    /**
+     * Allows the user to know if all tuple fields are not null
+     * @return {@code true} if all tuple elements are not null
+     */
+    public abstract boolean isFullFilled();
+
+    /**
+     * Returns the current tuple elements in a List
+     * @return A {@code List<Object>} with all elements on this tuple
+     */
     public List<Object> asList()
     {
         return new LinkedList<Object>();
     }
+
 }
